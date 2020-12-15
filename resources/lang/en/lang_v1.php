c6582e99201cedf994bdbc5b686ccb3cde02a7ad
Neet693 <neet693@gmail.com>
2020-12-15 16:12:02 +0800

    first commit

---
+++ resources/lang/en/lang_v1.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'category_page'=>'Halaman Kategori',
+    'category_panel'=>'Kategori',
+    'category_create'=>'Buat Kategori',
+    'categor_edit'=>'Ubah Kategori'
+];
